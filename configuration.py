3dbc24682d4b313f54d89dcc6a1057da43131fa3
Sean Boerhout <boeseany@gmail.com>
2021-10-22 21:18:01 -0700

    added frame margin

--- configuration.py
+++ configuration.py
@@ -1,21 +1,22 @@
 from enum import Enum
 
 class Settings(Enum):
-  FRAME_WIDTH  = 1000
-  FRAME_HEIGHT = 700
+  FRAME_WIDTH  = 800
+  FRAME_HEIGHT = 500
+  FRAME_MARGIN = 150
 
 # Character settings
 class GameSettings(Enum):
-  GAME_LOOPS            = 100
+  GAME_LOOPS            = 1000
   PREDATOR_COLOR = "red"
   PREY_COLOR     = "blue"
-  CharacterWidth = 5
+  CharacterWidth = 2
 
 class CharacterDefaults(Enum):
-  PREY_MOVEMENT_RADIUS  = 10
-  PREY_SIGHT_RADIUS = 75
-  PREDATOR_MOVEMENT_RADIUS = 15
-  PREDATOR_SIGHT_RADIUS = 50
+  PREY_MOVEMENT_RADIUS  = 7
+  PREY_SIGHT_RADIUS = 35
+  PREDATOR_MOVEMENT_RADIUS = 11
+  PREDATOR_SIGHT_RADIUS = 25
 
 class CharacterRole(Enum):
   PREDATOR = 0
--- main.py
+++ main.py
@@ -7,13 +7,25 @@ from Character import Character, Predator
 
 FRAME_WIDTH  = Settings.FRAME_WIDTH.value
 FRAME_HEIGHT = Settings.FRAME_HEIGHT.value
+FRAME_MARGIN = Settings.FRAME_MARGIN.value
+
+def getRandomCharacterPosition(x_or_y):
+  if x_or_y == "x":
+    return rnd.randint(FRAME_MARGIN, FRAME_MARGIN + FRAME_WIDTH)
+  elif x_or_y == "y":
+    return rnd.randint(FRAME_MARGIN, FRAME_MARGIN + FRAME_HEIGHT)
+
 
 def drawCharacters(window, characters):
   # refill the window
-  window_background = Rectangle(Point(0, 0), Point(Settings.FRAME_WIDTH.value, Settings.FRAME_HEIGHT.value))
+  window_background = Rectangle(Point(0, 0), Point(FRAME_WIDTH + 2 * FRAME_MARGIN, FRAME_HEIGHT + 2 * FRAME_MARGIN))
   window_background.setFill("white")
   window_background.setOutline("black")
   window_background.draw(window)
+
+  frame = Rectangle(Point(FRAME_MARGIN, FRAME_MARGIN), Point(FRAME_MARGIN + FRAME_WIDTH, FRAME_MARGIN + FRAME_HEIGHT))
+  frame.setOutline("black")
+  frame.draw(window)
 
   for character in characters:
     c = Circle(Point(character.posX, character.posY), GameSettings.CharacterWidth.value)
@@ -21,11 +33,11 @@ def drawCharacters(window, characters):
     if character.role == CharacterRole.PREDATOR:
       c2 = Circle(Point(character.posX, character.posY), CharacterDefaults.PREDATOR_SIGHT_RADIUS.value)
       c2.setOutline("red")
-      c2.draw(window)
+      # c2.draw(window)
     if character.role == CharacterRole.PREY:
       c2 = Circle(Point(character.posX, character.posY), CharacterDefaults.PREY_SIGHT_RADIUS.value)
       c2.setOutline("blue")
-      c2.draw(window)
+      # c2.draw(window)
     c.draw(window)
 
 
@@ -35,10 +47,10 @@ def runSim(window):
   prey = []
 
   # This is initial creation of the characters
-  for x in range(0, rnd.randint(3, 5)):
-    predators.append(Predator(CharacterRole.PREDATOR, rnd.randint(0, FRAME_WIDTH), rnd.randint(0, FRAME_HEIGHT)))
-  for x in range(0, rnd.randint(10, 15)):
-    prey.append(Character(CharacterRole.PREY, rnd.randint(0, FRAME_WIDTH), rnd.randint(0, FRAME_HEIGHT)))
+  for x in range(0, rnd.randint(30, 35)):
+    predators.append(Predator(CharacterRole.PREDATOR, getRandomCharacterPosition("x"), getRandomCharacterPosition("y")))
+  for x in range(0, rnd.randint(75, 100)):
+    prey.append(Character(CharacterRole.PREY, getRandomCharacterPosition("x"), getRandomCharacterPosition("y")))
 
   for cycle in range(0, GameSettings.GAME_LOOPS.value):
 
@@ -54,9 +66,9 @@ def runSim(window):
           prey.remove(victim)
 
       if (not has_victim) and predator.has_victim:
-        predator.setTargetStatus(has_victim, rnd.randint(0, FRAME_WIDTH), rnd.randint(0, FRAME_WIDTH))
+        predator.setTargetStatus(has_victim, getRandomCharacterPosition("x"), getRandomCharacterPosition("y"))
       elif (not has_victim) and (not predator.has_victim) and (predator.posX == predator.targetX) and (predator.posY == predator.targetY):
-        predator.setTargetStatus(has_victim, rnd.randint(0, FRAME_WIDTH), rnd.randint(0, FRAME_WIDTH))
+        predator.setTargetStatus(has_victim, getRandomCharacterPosition("x"), getRandomCharacterPosition("y"))
 
       predator.moveByPoint(predator.targetX, predator.targetY, True)
 
@@ -66,14 +78,14 @@ def runSim(window):
         if victim.canSee(predator.posX, predator.posY):
           victim.moveByPoint(predator.posX, predator.posY, False)
 
-    time.sleep(2)
+    # time.sleep(2)
 
     characters = predators + prey
     drawCharacters(window, characters)
     Text(Point(30, 10), f"Cycle: {cycle}").draw(window)
 
 def main():
-  window = GraphWin("Predators", Settings.FRAME_WIDTH.value, Settings.FRAME_HEIGHT.value)
+  window = GraphWin("Predators", Settings.FRAME_WIDTH.value + 2 * FRAME_MARGIN, Settings.FRAME_HEIGHT.value + 2 * FRAME_MARGIN)
   runSim(window)
   time.sleep(5)
   window.close()
